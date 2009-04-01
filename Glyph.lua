6d84ea241f28591d90714ed080a80ee3b74d3671
Meng Xiangliang <911mxl@gmail.com>
2009-04-01 23:29:59 +0800

    Implement "get ink prices from Auctioneer's herb market prices"

--- Glyph.lua
+++ Glyph.lua
@@ -1,4 +1,4 @@
-local parchmentPrice = {
+local reagentPrice = {
   [39354] = 12,		-- Light Parchment
   [10648] = 100,	-- Common Parchment
   [39501] = 1000,	-- Heavy Parchment
@@ -26,18 +26,44 @@ function TradeHelper:PickGlyph(lowestProfit)
         for reagentIndex=1, GetTradeSkillNumReagents(recipeIndex) do
           local reagentName, _, reagentCount, _ = GetTradeSkillReagentInfo(recipeIndex, reagentIndex)
           local reagentId = Enchantrix.Util.GetItemIdFromLink(GetTradeSkillReagentItemLink(recipeIndex, reagentIndex))
-          if parchmentPrice[reagentId] then
-            -- Parchment
-            cost = cost + parchmentPrice[reagentId] * reagentCount
-        self:Print(ChatFrame2, reagentName.." - "..parchmentPrice[reagentId])
-          else
-            -- Ink
-            
-            cost = cost + 1
-          end
+          cost = cost + reagentPrice[reagentId] * reagentCount
         end
         local profit = productPrice * productCount - cost
       end
     end
   end
+end
+
+function TradeHelper:GetInkPrice(marketPercent)
+  if marketPercent == nil then marketPercent = 1 end
+  
+  -- Herb to pigment
+  local pigmentPrice = {}
+  for herbId, group in pairs(Enchantrix.Constants.MillableItems) do
+    for pigmentId, millCount in pairs(Enchantrix.Constants.MillGroupYields[group]) do
+      local _, link = GetItemInfo(herbId)
+      local herbPrice = AucAdvanced.API.GetMarketValue(link)
+      if herbPrice then
+        millPrice = herbPrice * marketPercent * 5 / millCount
+        if pigmentPrice[pigmentId] == nil or pigmentPrice[pigmentId] > millPrice then
+          pigmentPrice[pigmentId] = millPrice
+        end
+      end
+    end
+  end
+  
+  -- Pigment to ink
+  CastSpellByName("Inscription")
+  for recipeIndex=1, GetNumTradeSkills() do
+    local name, type, _, _, _ = GetTradeSkillInfo(recipeIndex)
+    if strfind(name, "Ink") then
+      local inkId = Enchantrix.Util.GetItemIdFromLink(GetTradeSkillItemLink(recipeIndex))
+      local inkCount = GetTradeSkillNumMade(recipeIndex)
+      local _, _, pigmentCount, _ = GetTradeSkillReagentInfo(recipeIndex, 1)
+      local pigmentId = Enchantrix.Util.GetItemIdFromLink(GetTradeSkillReagentItemLink(recipeIndex, 1))
+      reagentPrice[inkId] = pigmentPrice[pigmentId] * pigmentCount / inkCount
+      self:Print(GetTradeSkillItemLink(recipeIndex)..reagentPrice[inkId])
+    end
+  end
+  CloseTradeSkill()
 end
